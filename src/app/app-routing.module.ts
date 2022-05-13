3c237324806418d40af972c766aec1b1bb2a3943
sanjusiva <snjusiru123@gmail.com>
2022-05-13 15:28:11 +0530

    nested

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChildComponent } from './child/child.component';
+import { GrandParentComponent } from './grand-parent/grand-parent.component';
+import { ParentComponent } from './parent/parent.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: 'grand', component: GrandParentComponent,
+  children: [
+    { path: 'parent', component: ParentComponent ,
+    children: [
+      { path: 'child', component: ChildComponent }
+    ]}
+  ]
+},
+{ path: '**', redirectTo: 'grand' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
+import { GrandParentComponent } from './grand-parent/grand-parent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    ParentComponent,
+    ChildComponent,
+    GrandParentComponent  ],
   imports: [
     BrowserModule,
     AppRoutingModule
